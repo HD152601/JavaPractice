efa242c0719c2da9bdf8de5d94a9d1e9634cf83e
ksw10 <ksw10@DESKTOP-36AKHQ0>
2017-05-12 09:20:07 +0900

    .

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,46 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ PersonTest
+ *
+ * 1. 개요 :
+ * 2. 작성일 : 2017. 5. 8.
+ * </pre>
+ *
+ * @author            : ksw10
+ * @version           : 1.0
+ */
+public class PersonTest2 {
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		
+		Person[] p = {
+				new Person("Tom"),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍")
+		};
+		for(Person person : p)
+		{
+			greeting(person);
+			System.out.println();
+		}
+		
+	}
+	private static void greeting(Person p){
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+	}
+	
+
+}
